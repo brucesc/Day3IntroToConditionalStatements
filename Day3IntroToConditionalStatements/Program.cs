e56a00cc6bf8a3ef776501d8ce84889072b42183
Steven Bruce <Brucesc726@gmail.com>
2018-02-23 15:36:12 -0500

    initial

--- Day3IntroToConditionalStatements/Program.cs
+++ Day3IntroToConditionalStatements/Program.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Day3IntroToConditionalStatements
+class Solution
 {
-    class Program
+
+    static void Main(String[] args)
     {
-        static void Main(string[] args)
+        int N = Convert.ToInt32(Console.ReadLine());
+
+        int weird = 0;
+        int notWeird = 0;
+       
+        if (N % 2 != 0)
         {
+            weird += 1;
         }
+        else if (N % 2 == 0 && 2 <= N && N < 5)
+        {
+            notWeird += 1;
+        }
+        else if (N % 2 == 0 && 6 <= N && N <= 20)
+        {
+            weird += 1;
+        }
+        else if (N % 2 == 0 && 20 < N)
+        {
+            notWeird += 1;
+        }
+
+        if (weird == 1)
+        {
+            Console.WriteLine("Weird");
+        }
+        else
+        {
+            Console.WriteLine("Not Weird");
+        }
+        Console.ReadKey();
+
     }
 }
